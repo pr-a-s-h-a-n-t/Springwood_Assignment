dc9312bf937213c5ccb0c0bb62d8f3c985ff8df9
Prashant Mishra <prashantmishra111@outlook.com>
2023-06-14 21:10:50 +0530

    bug fix in forecast!

--- assets/index.js
+++ assets/index.js
@@ -21,12 +21,13 @@ function getWeather() {
   fetch(apiUrl)
     .then((response) => {
       if (!response.ok) {
-        throw new Error("Please enter a valid location..");
+        throw new Error("Please enter a valid location.");
       }
       return response.json();
     })
     .then((data) => {
       displayWeather(data);
+      console.log(data);
     })
     .catch((error) => {
       showError(error.message);
@@ -41,9 +42,9 @@ function displayWeather(data) {
   var temperatureUnit = unitSwitch.value;
 
   var createWeatherItem = (
-    title,
     date,
-    temperature,
+    minTemp,
+    maxTemp,
     description,
     humidity,
     windSpeed,
@@ -56,83 +57,101 @@ function displayWeather(data) {
     var iconUrl = `https://openweathermap.org/img/wn/${icon}.png`;
 
     weatherElement.innerHTML = `
-    <img src="${iconUrl}" alt="Weather Icon" />
-    <div class="weather-item__title">
-      <h2>${title}</h2>
-      <h3>${main}</h3>
-    </div>
-    <h6>${description}</h6>
-    <p>Date: ${date}</p>
-    <p>Temperature: ${temperature}${temperatureUnit}</p>
-    <p>Humidity: ${humidity}%</p>
-    <p>Wind Speed: ${windSpeed} mph</p>
+      <img src="${iconUrl}" alt="Weather Icon" />
+      <div class="weather-item__title">
+        <h3>${date}</h3>
+        <h4>${main}</h4>
+      </div>
+      <h6>${description}</h6>
+      <p>Temperature: Min ${minTemp}${temperatureUnit}, Max ${maxTemp}${temperatureUnit}</p>
+      <p>Humidity: ${humidity}%</p>
+      <p>Wind Speed: ${windSpeed} mph</p>
     `;
 
     return weatherElement;
   };
 
-  var currentTemperature = data.list[0].main.temp;
-  var currentDescription = data.list[0].weather[0].description;
-  var currentHumidity = data.list[0].main.humidity;
-  var currentWindSpeed = data.list[0].wind.speed;
-  var currentWeatherIconCode = data.list[0].weather[0].icon;
-  var currentWeatherMain = data.list[0].weather[0].main;
-  var forecastDate = new Date(data.list[0].dt_txt).toDateString();
+  var forecastData = {};
 
-  var currentWeatherInfo = createWeatherItem(
-    "Current Weather",
-    forecastDate,
-    convertTemperature(currentTemperature),
-    currentDescription,
-    currentHumidity,
-    currentWindSpeed,
-    currentWeatherIconCode,
-    currentWeatherMain
-  );
-
-  weatherDisplay.appendChild(currentWeatherInfo);
-
-  var forecastList = data.list.slice(0, 5);
-
-  var forecastInfo = document.createElement("div");
-  forecastInfo.classList.add("forecast-container");
-  forecastInfo.innerHTML = "<h2>5-Day Forecast</h2>";
-
-  forecastList.forEach((forecast) => {
+  // Group forecast data by date
+  data.list.forEach((forecast) => {
     var forecastDate = new Date(forecast.dt_txt).toDateString();
-    var forecastTemperature = forecast.main.temp;
-    var forecastDescription = forecast.weather[0].description;
-    var forecastHumidity = forecast.main.humidity;
-    var forecastWindSpeed = forecast.wind.speed;
-    var forecastWeatherIconCode = forecast.weather[0].icon;
-    var forecastWeatherMain = forecast.weather[0].main;
 
-    var forecastItem = createWeatherItem(
-      forecastDate,
-      forecastDate,
-      convertTemperature(forecastTemperature),
-      forecastDescription,
-      forecastHumidity,
-      forecastWindSpeed,
-      forecastWeatherIconCode,
-      forecastWeatherMain
-    );
-
-    forecastInfo.appendChild(forecastItem);
+    if (!forecastData[forecastDate]) {
+      forecastData[forecastDate] = {
+        date: forecastDate,
+        minTemp: Number(forecast.main.temp_min - 273.15).toFixed(1),
+        maxTemp: Number(forecast.main.temp_max - 273.15).toFixed(1),
+        description: forecast.weather[0].description,
+        humidity: forecast.main.humidity,
+        windSpeed: forecast.wind.speed,
+        icon: forecast.weather[0].icon,
+        main: forecast.weather[0].main,
+      };
+    }
   });
 
-  weatherDisplay.appendChild(forecastInfo);
-}
+  // Get city name from API response
+  var cityName = data.city.name;
 
-function convertTemperature(temp) {
-  var unitSwitch = document.getElementById("unitSelect");
-  var isCelsius = unitSwitch.value === "C";
+  // Create container for current weather
+  var currentWeatherContainer = document.createElement("div");
+  currentWeatherContainer.classList.add("current-weather-container");
 
-  if (isCelsius) {
-    return Math.round(temp - 273.15);
-  } else {
-    return Math.round(((temp - 273.15) * 9) / 5 + 32);
-  }
+  // Create heading tag for current weather
+  var currentWeatherHeading = document.createElement("h2");
+  currentWeatherHeading.textContent = "Current Weather in " + cityName;
+
+  currentWeatherContainer.appendChild(currentWeatherHeading);
+
+  // Create current weather card
+  var currentWeather = data.list[0];
+  var currentWeatherItem = createWeatherItem(
+    "Today",
+    Number(currentWeather.main.temp_min - 273.15).toFixed(1),
+    Number(currentWeather.main.temp_max - 273.15).toFixed(1),
+    currentWeather.weather[0].description,
+    currentWeather.main.humidity,
+    currentWeather.wind.speed,
+    currentWeather.weather[0].icon,
+    currentWeather.weather[0].main
+  );
+
+  currentWeatherContainer.appendChild(currentWeatherItem);
+
+  // Append current weather container to the main weather display container
+  weatherDisplay.appendChild(currentWeatherContainer);
+
+  // Create container for forecasted weather cards
+  var forecastContainer = document.createElement("div");
+  forecastContainer.classList.add("forecast-container");
+
+  // Create heading tag for forecasted weather
+  var forecastHeading = document.createElement("h2");
+  forecastHeading.textContent = "Next 5-Days Forecast";
+
+  forecastContainer.appendChild(forecastHeading);
+
+  // Create forecast cards for the next 5 days
+  var forecastDates = Object.keys(forecastData).slice(1, 6); // Exclude the current day
+  forecastDates.forEach((date) => {
+    var forecast = forecastData[date];
+    var forecastItem = createWeatherItem(
+      forecast.date,
+      forecast.minTemp,
+      forecast.maxTemp,
+      forecast.description,
+      forecast.humidity,
+      forecast.windSpeed,
+      forecast.icon,
+      forecast.main
+    );
+
+    forecastContainer.appendChild(forecastItem);
+  });
+
+  // Append forecast container to the main weather display container
+  weatherDisplay.appendChild(forecastContainer);
 }
 
 function showError(message) {
